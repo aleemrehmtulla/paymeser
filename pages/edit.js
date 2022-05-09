367ba62db4f11ec1765f4e2a2968b6089ae52d5e
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-05-08 23:21:14 -0700

    Import fix

--- pages/edit.js
+++ pages/edit.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "../../utils/supabaseClient";
+import { supabase } from "../utils/supabaseClient";
 
 import {
   Box,
